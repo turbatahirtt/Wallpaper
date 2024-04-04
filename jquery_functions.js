f741a0eece8c1f8b5f7b496629b1a0abdbd2f441
turbatahirtt <160163736+turbatahirtt@users.noreply.github.com>
2024-04-04 18:26:15 -0400

    Final submission

--- jquery_functions.js
+++ jquery_functions.js
@@ -5,6 +5,8 @@ jQuery(function() {
     "nature 2": "nature2",
     "urban 1": "urban1",
     "urban 2": "urban2",
+    "desert 1": "desert1",
+    "desert 2": "desert2",
   }
 
   $('.dropdown-submenu a.test').on("click", function(e){
@@ -39,8 +41,16 @@ jQuery(function() {
     }
   })
 
+  $('.moon').on("click", function(e) {
+    if (background === "Dark") {
+      $(".main").css("background", "linear-gradient(to bottom, #071B26 60%, #FCF8E3 90%)")
+    } else {
+      $(".main").css("background", "linear-gradient(to bottom, #87CEEB 30%, #FCF8E3 80%)")
+    }
+  })
+
   $('.image').on("click", function(e) {
-    let image = e.target.innerHTML.toLowerCase();
+    let image = e.target.innerHTML.toLowerCase().replace(" ", "");
     $(".pattern").css("background-image", "url(" + image + ".png)");
     $(".pattern").css("background-repeat", "no-repeat");
     $(".pattern").css("background-attachment", "fixed");
@@ -53,6 +63,14 @@ jQuery(function() {
     $(".skyline-container").css("background-repeat", "no-repeat");
     $(".skyline-container").css("background-attachment", "fixed");
     $(".skyline-container").css("background-size", "100% 100%");
+  })
+
+  $('.animals').on("click", function(e) {
+    let image = e.target.innerHTML.toLowerCase();
+    $(".animals-container").css("background-image", "url(" + image + ".png)");
+    $(".animals-container").css("background-repeat", "no-repeat");
+    $(".animals-container").css("background-attachment", "fixed");
+    $(".animals-container").css("background-size", "100% 100%");
   })
 
 })
